65066ba6f4d4ca381e8f77482d555778e8fd8349
wolven531 <anthony.williams.cs@gmail.com>
2022-01-31 22:20:05 -0700

    Load initial poke count

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,52 @@
-import React, { FC } from 'react'
+import React, { FC, useCallback, useEffect, useState } from 'react'
+import { MainClient, NamedAPIResource } from 'pokenode-ts'
 import './App.css'
 
 export const App: FC = () => {
-	
+	const api = new MainClient()
+
+	const [isCountLoaded, setIsCountLoaded] = useState(false)
+	const [pokemon, setPokemon] = useState<NamedAPIResource[]>([])
+	const [totalCount, setTotalCount] = useState(0)
+
+	const fetchPokemonCount = useCallback(async () => {
+		if (isCountLoaded) {
+			return Promise.resolve()
+		}
+
+		return api.pokemon
+			.listPokemons(undefined, 100 * 20)
+			.then((result) => {
+				setTotalCount(result.count)
+				setPokemon(result.results as NamedAPIResource[])
+			})
+			.catch((err) => {
+				console.error(`Problem fetching pokemon count - ${err}`)
+			})
+			.finally(() => {
+				setIsCountLoaded(true)
+			})
+	}, [api.pokemon, isCountLoaded])
+
+	useEffect(() => {
+		fetchPokemonCount()
+	}, [fetchPokemonCount])
+
 	return (
 		<article className="app">
 			<header>
 				<h2>Pokedex Nova</h2>
 			</header>
-			<section></section>
+			{isCountLoaded && (
+				<section>
+					<div>Total count - {totalCount}</div>
+					{pokemon.map(({ name, url }) => (
+						<div key={url}>
+							<a href={url}>{name}</a>
+						</div>
+					))}
+				</section>
+			)}
 			<footer>&copy; Anthony Williams 2022</footer>
 		</article>
 	)
