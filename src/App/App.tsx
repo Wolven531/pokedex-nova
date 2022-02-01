334d4824d00bc76d6530ec7d0473aa9e375a8c80
wolven531 <anthony.williams.cs@gmail.com>
2022-01-31 21:48:21 -0700

    Clean up App

---
+++ src/App/App.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react'
+import './App.css'
+
+export const App: FC = () => {
+	
+	return (
+		<article className="app">
+			<header>
+				<h2>Pokedex Nova</h2>
+			</header>
+			<section></section>
+			<footer>&copy; Anthony Williams 2022</footer>
+		</article>
+	)
+}
