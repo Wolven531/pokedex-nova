c9ea2356b3740f334f0c6e4293e22054d1bb0f77
wolven531 <anthony.williams.cs@gmail.com>
2022-02-02 20:14:15 -0700

    Fix test warnings, add loaded snapshot

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,19 +1,31 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { render, RenderResult, waitFor } from '@testing-library/react'
 import { App } from './App'
 
 describe('App', () => {
 	let comp: RenderResult
 
 	beforeEach(() => {
-		comp = render(<App />)
-	})
+		// TODO - mock network / poke client
 
-	it('renders header', () => {
-		expect(comp.getByText(/Pokedex Nova/)).toBeInTheDocument()
+		comp = render(<App />)
 	})
 
 	it('matches snapshot', () => {
 		expect(comp.asFragment()).toMatchSnapshot()
+	})
+
+	describe('when loaded', () => {
+		beforeEach(async () => {
+			await waitFor(() => comp.getByText(/Total pokemon count -/))
+		})
+
+		it('renders header', () => {
+			expect(comp.getByText(/Pokedex Nova/)).toBeInTheDocument()
+		})
+
+		it('matches snapshot', () => {
+			expect(comp.asFragment()).toMatchSnapshot()
+		})
 	})
 })
