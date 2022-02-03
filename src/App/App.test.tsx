b8f75c98b7cc35b4d47f6083f050dae8327ffc41
wolven531 <anthony.williams.cs@gmail.com>
2022-02-02 20:37:03 -0700

    Update tests

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,14 +1,41 @@
 import React from 'react'
 import { render, RenderResult, waitFor } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
+
+// !!! this did NOT work due to jest Mock
+// const mockListPokemons = jest.fn().mockResolvedValue({
+// 	count: 99,
+// })
+// !!! this did NOT work due to jest Mock
+// const mockListPokemons = jest.fn(() => Promise.resolve({ count: 99 }))
+
+// !!! this ruins the loading snapshot
+// const FAKE_COUNT = 99
+
+// MUST mock BEFORE importing App
+// jest.mock('pokenode-ts', () => {
+// 	return {
+// 		MainClient: function () {
+// 			return {
+// 				pokemon: {
+// 					// listPokemons: mockListPokemons,
+// 					listPokemons: () => Promise.resolve({ count: FAKE_COUNT }),
+// 				},
+// 			}
+// 		},
+// 	}
+// })
 import { App } from './App'
 
 describe('App', () => {
 	let comp: RenderResult
 
 	beforeEach(() => {
-		// TODO - mock network / poke client
+		// TODO - mock network (NOT poke client)
 
-		comp = render(<App />)
+		act(() => {
+			comp = render(<App />)
+		})
 	})
 
 	it('matches snapshot', () => {
@@ -17,7 +44,10 @@ describe('App', () => {
 
 	describe('when loaded', () => {
 		beforeEach(async () => {
-			await waitFor(() => comp.getByText(/Total pokemon count -/))
+			await waitFor(
+				() => comp.getByText(/Total pokemon count -/),
+				// () => comp.getByText(`Total pokemon count - ${FAKE_COUNT}`),
+			)
 		})
 
 		it('renders header', () => {
